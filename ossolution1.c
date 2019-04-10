cea17a90a503523635d90b8abaa33012924230c1
manav <manavisright@gmail.com>
2019-04-10 20:14:38 +0530

    printing prime numbers with threads

--- ossolution1.c
+++ ossolution1.c
@@ -1,12 +1,12 @@
 // printing prime numbers less than or equal to a given number
 #include<stdio.h>
-void main()
-{
-    int number,count;
-    printf("Enter the Number: ");
-    scanf("%d",&number);
-    printf("Prime Numbers are: \n");
-    for(int i=1; i<=number; i++)
+#include<pthread.h>
+
+int number,count;
+
+void *show(void *n){
+
+	for(int i=1; i<=number; i++)
     {
         count=0;
         for(int j=1; j<=i; j++)
@@ -16,7 +16,24 @@ void main()
         }
         if(count==2)
             printf("%d " ,i);
-    }
+    }// end for loop
+	
+}
+
+void main()
+{
+    
+    printf("Enter the Number: ");
+    scanf("%d",&number);
+    printf("Prime Numbers are: \n");
+    
+    
+    
+    pthread_t tid1;
+	
+	pthread_create(&tid1, NULL,&show, NULL);
+	pthread_join(tid1,NULL);
+   
 	
    printf("\n");
 }
